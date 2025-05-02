694bf1b6445f1e31ba65609a7fba2ab68bc57929
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-05-02 10:29:48 +0200

    feat: export CSV file from detected issues.

--- validation/maganamed_validation.py
+++ validation/maganamed_validation.py
@@ -4,24 +4,24 @@ import yaml
 from validation.general_validation import DataValidator
 
 VALID_SITE_CODES_AND_CENTER_NAMES = {
-        1: 'Lothian',
-        2: 'Camhs',
-        3: 'Mannheim',
-        4: 'Wiesloch',
-        5: 'Leuven',
-        6: 'Bierbeek',
-        7: 'Bratislava',
-        8: 'Kosice'
-    }
+    1: 'Lothian',
+    2: 'Camhs',
+    3: 'Mannheim',
+    4: 'Wiesloch',
+    5: 'Leuven',
+    6: 'Bierbeek',
+    7: 'Bratislava',
+    8: 'Kosice'
+}
 
-output_csv_path="validation_issues.csv"
+output_csv_path = "validation_issues.csv"
+
 
 class MaganamedValidation:
 
     def __init__(self, df):
         self.magana_df = df
         self.magana_issues = []
-
 
     def validate_site_and_center_name_id(self, site_column, center_name_column, study_id_column):
 
@@ -33,7 +33,8 @@ class MaganamedValidation:
         # Validation of participant_ID & Center Name
         self.magana_df['id_validation_result'] = self.magana_df.apply(
             lambda row: 'OK' if row['abbreviation_center_name'] in row[study_id_column] else 'ID-mismatch', axis=1)
-        results = self.magana_df[[study_id_column, site_column, center_name_column,'abbreviation_center_name','id_validation_result']]
+        results = self.magana_df[
+            [study_id_column, site_column, center_name_column, 'abbreviation_center_name', 'id_validation_result']]
         filter_id_issues = results[results.id_validation_result == 'ID-mismatch']
 
         if not filter_id_issues.empty:
@@ -46,8 +47,10 @@ class MaganamedValidation:
         self.magana_df[site_column] = self.magana_df[site_column]
         normalised_control_dict = {k: v.upper() for k, v in VALID_SITE_CODES_AND_CENTER_NAMES.items()}
         self.magana_df['site_validation_result'] = self.magana_df.apply(
-            lambda row: 'OK' if normalised_control_dict.get(row[site_column]) == row[center_name_column] else 'Site-mismatch',axis=1)
-        site_issues = self.magana_df[[study_id_column, site_column, center_name_column, 'abbreviation_center_name', 'site_validation_result']]
+            lambda row: 'OK' if normalised_control_dict.get(row[site_column]) == row[
+                center_name_column] else 'Site-mismatch', axis=1)
+        site_issues = self.magana_df[
+            [study_id_column, site_column, center_name_column, 'abbreviation_center_name', 'site_validation_result']]
         filter_site_issues = site_issues[site_issues['site_validation_result'] == 'Site-mismatch']
 
         if not site_issues.empty:
@@ -58,12 +61,12 @@ class MaganamedValidation:
 
         if self.magana_issues:
             all_issues_df = pd.concat(self.magana_issues, ignore_index=True)
-            all_issues_df = all_issues_df.groupby([study_id_column,site_column, center_name_column], as_index=False).first()
+            all_issues_df = all_issues_df.groupby([study_id_column, site_column, center_name_column],
+                                                  as_index=False).first()
             all_issues_df.to_csv(output_csv_path, sep=';', index=False)
             print(f"\n Issues exported to '{output_csv_path}' file.")
         else:
             print("\n ✔ | All validations passed. No issues found.")
-
 
     def validate_special_duplication_types(self, column):
         issues = []
