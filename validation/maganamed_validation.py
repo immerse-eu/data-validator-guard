c783452e0c79dcc58bfd4bbd85c545c4a5cb30bb
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-05-06 18:18:44 +0200

    feat: update maganamed_validation.py. Add auxiliar method "import_custom_csr_df_with_language_selection"

--- validation/maganamed_validation.py
+++ validation/maganamed_validation.py
@@ -4,17 +4,44 @@ import yaml
 from validation.general_validation import DataValidator
 
 VALID_SITE_CODES_AND_CENTER_NAMES = {
-    1: 'Lothian',
-    2: 'Camhs',
-    3: 'Mannheim',
-    4: 'Wiesloch',
-    5: 'Leuven',
-    6: 'Bierbeek',
-    7: 'Bratislava',
-    8: 'Kosice'
+    1: 'Lothian',   # L0
+    2: 'Camhs',     # L
+    3: 'Mannheim',  # L1
+    4: 'Wiesloch',  # L1
+    5: 'Leuven',    # L2
+    6: 'Bierbeek',  # L2
+    7: 'Bratislava',# L3
+    8: 'Kosice'     # L3
 }
 
+VALID_LANGUAGE_SELECTION = {
+    0: 'English',
+    1: 'German',
+    2: 'Belgian',
+    3: 'Slovak'
+    }
+
+VALID_PARTICIPANTS_TYPE = {
+    0: 'Patient',
+    1: 'Clinician',
+    2: ['Teamlead', 'Admin'], # Teamlead / Admin
+    3: ['Finance ', 'accounting staff'] # Finance / accounting staff
+    }
+
+
+
 output_csv_path = "validation_issues.csv"
+
+def import_custom_csr_df_with_language_selection():
+
+    with open("./config/config.yaml", "r", encoding="utf-8") as path:
+        config = yaml.safe_load(path)
+    csri = config['auxiliarFiles']['csri']
+
+    df = pd.read_csv(csri)
+    new_df = df.drop_duplicates()
+    new_df.to_csv("filter_crsi_file.csv", index=False)
+    return new_df
 
 
 class MaganamedValidation:
@@ -38,10 +65,10 @@ class MaganamedValidation:
         filter_id_issues = results[results.id_validation_result == 'ID-mismatch']
 
         if not filter_id_issues.empty:
-            print(f"\n❌ | Issues found in IDS:\n'{filter_id_issues}")
+            print(f"\n❌ {len(filter_id_issues)} | Issues have been found in participants IDs:\n {filter_id_issues}")
             self.magana_issues.append(filter_id_issues)
         else:
-            print("\n ✔ | Validation of IDS passed: Not issues in IDS found! }")
+            print("\n ✔ | Validation of IDS passed: No issues were detected in participant IDs! ")
 
         # Validation of Site
         self.magana_df[site_column] = self.magana_df[site_column]
@@ -53,11 +80,12 @@ class MaganamedValidation:
             [study_id_column, site_column, center_name_column, 'abbreviation_center_name', 'site_validation_result']]
         filter_site_issues = site_issues[site_issues['site_validation_result'] == 'Site-mismatch']
 
-        if not site_issues.empty:
-            print(f"\n❌ | Issues found in Site column :\n'{filter_site_issues}")
+
+        if not filter_site_issues.empty:
+            print(f"\n❌ {len(filter_site_issues)} | Issues have been found in 'Site' column :\n{filter_site_issues}")
             self.magana_issues.append(filter_site_issues)
         else:
-            print("\n✔ | Validation of IDS passed: No issues in 'Site' found! }")
+            print("\n ✔ | Validation of 'Site' passed: No issues were detected in 'Site' columns!")
 
         if self.magana_issues:
             all_issues_df = pd.concat(self.magana_issues, ignore_index=True)
@@ -66,13 +94,10 @@ class MaganamedValidation:
             all_issues_df.to_csv(output_csv_path, sep=';', index=False)
             print(f"\n Issues exported to '{output_csv_path}' file.")
         else:
-            print("\n ✔ | All validations passed. No issues found.")
+            print("\n ✔  | All validations were successfully passed!!")
 
     def validate_special_duplication_types(self, column):
         issues = []
-
-        general_validator = DataValidator(self.magana_df)
-        general_validator.check_duplicates(column)
 
         normalised_column = self.magana_df[column].str.strip()
         filter_normalised_column_with_additional_characters = normalised_column[
@@ -83,10 +108,16 @@ class MaganamedValidation:
 
         filter_issues = self.magana_df[self.magana_df['is_duplicate'] == True]
         if filter_issues.empty:
-            print(f"\n ✔ | Validation of special duplications passed: No duplications found in column '{column}'! '")
+            print(f"\n ✔ | Validation of special duplications passed: No duplications were found in column '{column}'.")
         else:
             print(f"\n❌ | Issues found in '{column}' column :\n'{filter_issues}")
             issues.append(filter_issues)
             return issues
 
-        print("\nAdditional observations: \n", filter_normalised_column_with_additional_characters)
+        print(f"\nAdditional observations from '{column}': \n", filter_normalised_column_with_additional_characters)
+
+    def validate_language_selection(self, column):
+        self.magana_df[column] = self.magana_df[column].str.strip().str.upper()
+
+    def passed_validation(self):
+        return len(self.magana_issues) == 0
