db682e9403eefec9e4f77ebe4edccfbd45c8081f
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-05-22 16:52:40 +0200

    feat: add manual duplication control

--- validation/testing/execute_test.py
+++ validation/testing/execute_test.py
@@ -3,12 +3,22 @@ from validation.testing import mock_data
 
 VALID_CITIES = {"New York", "London", "Paris"}
 
+ids_test = [] # Paste IDS post processed by GH
+
+def manual_ids_duplication_control():
+    control_ids = set()
+    for ide in ids_test:
+        if ide not in control_ids:
+            control_ids.add(ide)
+    print("no repetitive items: ", len(control_ids))
+    print("length original items", len(ids_test))
+
 
 def run_test_with_mock_data():
 
     # -- STEP 1. Read generated "Mock" data
     df = mock_data.generate_df()
-    print(df)
+    # print(df)
 
     # -- STEP 2. Run a general validation: typos, and duplicates
     validator = DataValidator(df)
@@ -16,5 +26,8 @@ def run_test_with_mock_data():
     validator.check_typos(column="city", dictionary=VALID_CITIES)
 
     # -- STEP 3. Output analysis report
-    report = validator.report()
-    print("\n Report: ", report)
+    validator.report()
+    # print("\n Report: ", report)
+
+run_test_with_mock_data()
+manual_ids_duplication_control()
