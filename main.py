76c09feea5779a22eccf276e0c0be1fb1a602c36
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-05-27 18:57:51 +0200

    feat: Add new class to check duplications in normalised columns
    feat: update check correct Ids

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ valid_center_names = VALID_SITE_CODES_AND_CENTER_NAMES.values()
 DB_PATH = load_config_file('researchDB','db_path')
 FIXES_PATH = load_config_file('reports','fixes')
 NEW_DB_PATH = load_config_file('researchDB','cleaned_db')
-ID_CONTROL_PATH = load_config_file('auxiliarFiles','ids_master_reference')
-ID_SAMPLE_PATH = load_config_file('auxiliarFiles','ids_master_test')
+ID_CONTROL_PATH = load_config_file('auxiliarFiles','ids_reference')
+ID_SAMPLE_PATH = load_config_file('auxiliarFiles','ids_to_verify')
 
 def connect_and_fetch_table(table_name):
     sql_connection = sqlite3.connect(NEW_DB_PATH)
@@ -30,7 +30,7 @@ def run_general_validation(table):
     print(f"\n\033[95m Validating General rules:\033[0m\n")
     general_magana_validation = DataValidator(table)
     general_magana_validation.check_typos(column="center_name", dictionary=valid_center_names)
-    general_magana_validation.check_duplicates(table)
+    general_magana_validation.check_general_duplications(table)
     general_magana_validation.report()
     is_approved = general_magana_validation.passed_validation()
     return is_approved
@@ -97,35 +97,54 @@ def run_auxiliary_rule_six (table):
 def general_validation_ids(df_control, df_to_validate):
     print(f"\n\033[95m Validating IDS :\033[0m\n")
     general_validation = DataValidator(df_to_validate)
-    general_validation.check_duplicates(df_to_validate)
-    general_validation.check_correct_ids(df_control, id_column='study_id_pat')
+    general_validation.check_general_duplications(df_to_validate)
+    general_validation.check_duplications_applying_normalisation('participant_identifier')
+    general_validation.check_correct_ids(df_control, id_column=0)
+
     # general_validation.check_typos(column="participant_identifier", dictionary=valid_center_names)
     # rules_magana_validation = MaganamedValidation(df_to_validate)
-    # rules_magana_validation.validate_special_duplication_types(column="study_id_pat")
+    # rules_magana_validation.validate_special_duplication_types(column="record_id")
 
-def run_rules_from_df(control_dir):
+# TODO: maintain this function enable a broader usage
+def run_rules_from_df(reference_directory, test_directory):
     df_control = None
 
-    for filename in os.listdir(control_dir):
-        if filename.endswith(".csv"):
-            if filename.startswith("I_all_TD_UK_120325"): # Control FILE
-                df_control = pd.read_csv(os.path.join(control_dir, filename))
-                print(f"\n\033[34mControl file: '{filename}' \033[0m\n")
-                print(df_control.info())
-            else:
-                csv_df = pd.read_csv(os.path.join(control_dir, filename))  # File(s) to validate
-                print(f"\n\033[34mFile to validate: '{filename}' \033[0m\n")
-                print(csv_df.info())
-                general_validation_ids(df_control, csv_df)
+    if os.path.exists(reference_directory):
+        df_control = pd.read_excel(reference_directory)
+        print(f"\n\033[34mControl file: '{os.path.basename(reference_directory)}' \033[0m\n")
+        print(df_control.info())
 
-        elif filename.endswith(".xlsx"):
-            excel_df = pd.read_excel(os.path.join(control_dir, filename)) # File(s) to validate
-            print(f"\n\033[34mFile to validate:'{filename}' \033[0m\n")
-            print(excel_df.head(3))
-            general_validation_ids(df_control, excel_df)
+        for filename in os.listdir(test_directory):
+            if filename.startswith("redcap_01_concatenated_only"):
+                if filename.endswith(".csv"):
+                    csv_df = pd.read_csv(os.path.join(test_directory, filename))  # File(s) to validate
+                    print(f"\n\033[34mFile to validate: '{filename}' \033[0m\n")
+                    # print(csv_df.info())
+                    general_validation_ids(df_control, csv_df)
+
+                elif filename.endswith(".xlsx"):
+                    excel_df = pd.read_excel(os.path.join(test_directory, filename)) # File(s) to validate
+                    print(f"\n\033[34mFile to validate:'{filename}' \033[0m\n")
+                    # print(excel_df.head(3))
+                    general_validation_ids(df_control, excel_df)
+    else:
+        print(f"\n\033[34mFilepath not found!\033[0m\n")
 
 
 def main():
+    # -- Rule 0: ID validation
+    df_control = pd.read_excel(ID_CONTROL_PATH)
+    for filename in os.listdir(ID_SAMPLE_PATH):
+        if filename.startswith("extracted"):        # Change according files
+            if filename.endswith(".csv"):
+                print(f"\n\033[34mFile to validate: '{filename}' \033[0m\n")
+                csv_df = pd.read_csv(os.path.join(ID_SAMPLE_PATH, filename))
+                general_validation_ids(df_control, csv_df)
+
+            elif filename.endswith(".xlsx"):
+                excel_df = pd.read_excel(os.path.join(ID_SAMPLE_PATH, filename))
+                print(f"\n\033[34mFile to validate: '{filename}' \033[0m\n")
+                general_validation_ids(df_control, excel_df)
 
     # -- Rule 1: Apply validation for 'Kind-of-participant'.
     read_kind_participants_df = connect_and_fetch_table("Kind-of-participant")
--- validation/general_validation.py
+++ validation/general_validation.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
+
 class DataValidator:
 
     def __init__(self, df):
         self.df = df
         self.issues = []
 
-    def check_duplicates(self, subset):
+    def check_general_duplications(self, subset):
         duplicates = self.df.loc[self.df.duplicated(subset=subset, keep=False)].copy()
         duplicates["issue_type"] = "duplication"
 
@@ -15,6 +16,34 @@ class DataValidator:
         else:
             print(f"\n❌ | {len(duplicates)} Duplicated values have been found in table:\n '{duplicates}'")
             self.issues.append(duplicates)
+
+    def check_duplications_applying_normalisation(self, column: str = None):
+        self.df = self.df.copy()
+
+        if column is not None:
+            column = self.df.columns[0]
+
+        def normalize_values_to_uppercase(value):
+            if isinstance(value, str):
+                return value.strip().upper()
+            elif pd.isna(value):
+                return value
+            else:
+                return value
+
+        normalized_column = f"{column}_normalized"
+        self.df[normalized_column] = self.df[column].apply(normalize_values_to_uppercase)
+        self.df["is_duplicate"] = self.df.duplicated(subset=[normalized_column], keep=False)
+        self.df["issue_type"] = "duplication"
+
+        filter_duplicates = self.df[self.df['is_duplicate'] == True]
+        # print(self.df[['participant_identifier', 'participant_identifier_normalized', 'is_duplicate']].head(3))
+
+        if filter_duplicates.empty:
+            print(f"\n ✔ | Validation of special duplications passed: No duplicated rows were found in current table.")
+        else:
+            print(f"\n❌ | {len(filter_duplicates)} Duplicated values have been found in current table.")
+            self.issues.append(filter_duplicates)
 
     def check_typos(self, column, dictionary):
         self.df[column] = self.df[column].str.strip().str.lower()
@@ -30,25 +59,27 @@ class DataValidator:
             self.issues.append(typos)
 
     def check_correct_ids(self, df_control, id_column):
-        self.df[id_column] = self.df[id_column].str.strip()
-        comparison_ids = set(self.df[id_column].dropna())
-        control_ids = set(df_control[id_column].dropna())
+        # self.df[id_column] = self.df[id_column].str.strip()
+        # comparison_ids = set(self.df[id_column].dropna())
+        # control_ids = set(df_control[id_column].dropna())
 
-        print(f" Control file length: {len(control_ids)}")
-        print(f" Test file length: {len(comparison_ids)}")
+        comparison_ids = set(self.df.iloc[:, 0].dropna().astype(str).str.strip())
+        control_ids = set(df_control.iloc[:, 0].dropna().astype(str).str.strip())
+
+        print(f"  | Control file length: {len(control_ids)}")
+        print(f"  | Test file length: {len(comparison_ids)}")
 
         missing_ids = control_ids - comparison_ids
         extra_ids = comparison_ids - control_ids
 
-        print(" Missing IDs in test file: ", list(missing_ids))
-        print(" Extra IDs in test file: ",   list(extra_ids))
+        print(f"  | {len(missing_ids)} Missing IDs in test file: ", list(missing_ids))
+        print(f"  | {len(extra_ids)} Extra IDs in test file: ", list(extra_ids))
 
-        len_discrepancies = self.df[
-            self.df[id_column].apply(lambda x: not isinstance(x, str) or len(x) != 10 if pd.notnull(x) else False)]
-
-        print(f"\nNumber of IDs discrepancies: {len(len_discrepancies[id_column])} ")
-        print(len_discrepancies[[id_column, 'site']])
-
+        # len_discrepancies = self.df[
+        #     self.df[id_column].apply(lambda x: not isinstance(x, str) or len(x) != 10 if pd.notnull(x) else False)]
+        #
+        # print(f"\nNumber of IDs discrepancies: {len(len_discrepancies[id_column])} ")
+        # print(len_discrepancies[[id_column, 'site']])
 
     def report(self):
         if self.issues:
