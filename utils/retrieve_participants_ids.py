7ff8fd26d7f1486b0926e321734988cd3e5d799b
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-07-11 10:48:10 +0200

    feat: update when table is not found in db when getting a df.

--- utils/retrieve_participants_ids.py
+++ utils/retrieve_participants_ids.py
@@ -24,22 +24,23 @@ def export_ids_per_table(df):
     return unique_ids
 
 
-def get_unique_participant_identifier_per_system(system):
+def get_unique_participant_identifier_per_system_db(system):
     unique_participant_identifiers = set()
     filtered_tablename_df = read_db_catalogue(DB_CATALOGUE_PATH, system)
     for tablename in filtered_tablename_df:
         print("Table name: ", tablename)
         sql_df = connect_and_fetch_table(tablename)
-        participant_identifiers = export_ids_per_table(sql_df)
-        unique_participant_identifiers.update(participant_identifiers)
+        if sql_df is not None:
+            participant_identifiers = export_ids_per_table(sql_df)
+            unique_participant_identifiers.update(participant_identifiers)
 
     print("Unique participant identifiers per system: ", len(unique_participant_identifiers))
     sorted_unique_participants = sorted(unique_participant_identifiers)
     unique_participants_df = pd.DataFrame(sorted_unique_participants, columns=['participant_identifier'])
-    output_filename = f'unique_identifiers_per_participant_from_{system}.csv'
+    output_filename = f'new_unique_identifiers_per_participant_from_{system}.csv'
     output_file = os.path.join(os.path.dirname(DB_CATALOGUE_PATH), output_filename)
     unique_participants_df.to_csv(output_file, sep=';', index=False)
     print("file exported")
 
 
-get_unique_participant_identifier_per_system('maganamed')
+get_unique_participant_identifier_per_system_db('maganamed')
