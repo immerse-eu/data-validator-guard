8080cd0779cd3c458358cad9f8070eb044ebd553
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-11-28 14:09:55 +0100

    feat: Add fidelity tailored rulebook function

--- utils/rulebook.py
+++ utils/rulebook.py
@@ -72,3 +72,34 @@ def add_site_codes_to_rulebook():
     filepath = os.path.join(path, file_name)
     merged_df.to_csv(filepath, sep=';', index=False)
     print("Successfully saved")
+
+
+def create_rulebook(extracted_ids_path):
+    reference_ids_df = get_columns_from_id_reference()
+    all_extracted_ids = []
+    original_extracted_ids = []
+
+    # List of files to process (replace with your actual file list)
+    files = os.listdir(extracted_ids_path)
+    for file in files:
+        if file.endswith(".xlsx") and 'fidelity' in file:
+            print(f"Processing file {file}")
+            df = pd.read_excel(os.path.join(extracted_ids_path, file))
+            df = df.rename(columns={df.columns[0]: 'correct_participant_identifier'})
+            df['participant_identifier_upper'] = df['correct_participant_identifier'].astype(str).str.strip().str.upper()
+            all_extracted_ids.append(df)
+
+    concat_extracted_ids = pd.concat(all_extracted_ids)
+    duplicates = concat_extracted_ids[concat_extracted_ids.duplicated()]
+    print("Numer duplicates found:", len(duplicates), duplicates)
+    concat_extracted_ids = concat_extracted_ids.drop_duplicates()
+    merged_df = reference_ids_df.merge(concat_extracted_ids, on='correct_participant_identifier', how='outer')
+    merged_df['action'] = merged_df['correct_participant_identifier'].apply(
+        lambda x: "delete" if " " in str(x) or "delete" in str(x) or "test" in str(x) else (
+            "update" if str(x).strip() and len(str(x).strip()) >= 10
+            else "check manually"))
+    merged_df.to_excel(os.path.join(extracted_ids_path, 'rulebook_fidelity.xlsx'), index=False)
+
+
+# fidelity = "V:/Abteilungen/ISA/DIZ/6 Transferstelle und Externe (DIZ-TEA)/FDM/immerse/Data sharing_intern/Nayeli/TEMPORAL/IMMERSE_Master_repository/IMMERSE_extracted_ids/id_extraction_post_processed_files"
+# create_rulebook(fidelity)
