3e4346b50645cd69724038530cfc7c921ec4af2d
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-12-08 16:10:27 +0100

    fix: rebase read function to apply changes in db.

--- cleaning/cleaning_db.py
+++ cleaning/cleaning_db.py
@@ -5,23 +5,26 @@ from config.config_loader import load_config_file
 
 FIXES_FILE_PATH = load_config_file('reports', 'fixes')
 CHANGES_FILE_PATH = load_config_file('reports', 'changes')
-DB_PATH = load_config_file('researchDB', 'db_path')
-NEW_DB_PATH = load_config_file('researchDB', 'clean_db')
+TEMPORAL_SQL_DB_DIR = load_config_file('researchDB', 'db_path')
+FINAL_SQL_DB_DIR = load_config_file('researchDB', 'clean_db')
 
 
 def get_all_tables(path_db):
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     table_names = [row[0] for row in cursor.fetchall()]
     return table_names
 
 
-def get_master_file(fixed_filepath):
-    for file in os.listdir(fixed_filepath):
-        if 'Kind' in file and file.endswith('.csv'):
-            return pd.read_csv(os.path.join(fixed_filepath, file))
+def get_changes_df_per_system(changes_filepath, sub_foder):
+    target_dir = os.path.join(changes_filepath, sub_foder)
+
+    if os.path.exists(target_dir):
+        for file in os.listdir(target_dir):
+            if file.startswith("05-12-2025_Kind") and file.endswith(".csv"): # TODO: Correct to the current file.
+                filepath = os.path.join(target_dir, file)
+                return pd.read_csv(filepath, sep=";")
 
 
 def has_column(conn, table_name, column_name):
@@ -89,25 +92,27 @@ def apply_changes(conn, table, change_type, row):
 
 
 def cleaning_db(path_db, system):
-    if system == 'maganamed':   # TODO: Verify and complete changes by system
-        changes_df = get_master_file(CHANGES_FILE_PATH)
+    db_path = next(os.path.join(path_db, file) for file in os.listdir(path_db) if file.endswith('.db'))
+
+    if system == 'maganamed':
+        changes_df = get_changes_df_per_system(CHANGES_FILE_PATH, 'changes_maganamed')
         changes_df = changes_df[["participant_identifier", "site_validation_result", "Expected_value"]]
 
-        #changes_df_by_id = changes_df[changes_df["id_validation_result"] == "ID-mismatch"]
+        # changes_df_by_id = changes_df[changes_df["id_validation_result"] == "ID-mismatch"]
         changes_df_by_center_name = changes_df[changes_df["Expected_value"] == "Camhs"]
         filter_by_int_values = pd.to_numeric(changes_df["Expected_value"], errors='coerce').notna()
         changes_df_by_site = changes_df[filter_by_int_values]
 
         change_sets = [
-            #("changes_df_by_id", changes_df_by_id),
+            # ("changes_df_by_id", changes_df_by_id),
             ("changes_df_by_center_name", changes_df_by_center_name),
             ("changes_df_by_site", changes_df_by_site)
         ]
 
-        conn = sqlite3.connect(path_db)
+        conn = sqlite3.connect(db_path)
 
         for change_type, df in change_sets:
-            for table in get_all_tables(path_db):
+            for table in get_all_tables(db_path):
                 for _, row in df.iterrows():
                     apply_changes(conn, table, change_type, row)
         conn.commit()
